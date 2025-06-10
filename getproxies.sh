a80ef84c3566becd17e1a6757f523071fc412f28
khanhhd1987 <75921387+khanhhd1987@users.noreply.github.com>
2025-06-10 11:24:38 +0700

    Add proxy fetch script

---
+++ getproxies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MAX=16
+
+for i in $(seq 1 $MAX); do
+  VM="proxy-vm-$i"
+
+  # Xen kẽ vùng southeast2-b và southeast1-b
+  if (( i % 2 == 1 )); then
+    ZONE="asia-southeast2-b"
+  else
+    ZONE="asia-southeast1-b"
+  fi
+
+  echo -n "$VM: "
+  gcloud compute ssh "$VM" \
+    --zone="$ZONE" \
+    --command="sudo cat /root/proxy.txt" \
+    --ssh-flag="-o StrictHostKeyChecking=no" \
+    --ssh-flag="-o UserKnownHostsFile=/dev/null" \
+    --quiet
+done
